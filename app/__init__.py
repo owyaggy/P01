f90946698bac0a1456e14ece370ea03e6061d939
Owen Yaggy <owyaggy@icloud.com>
2021-12-14 08:13:55 -0500

    import routes from routes.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,7 @@ import os
 app = Flask(__name__)    #create Flask object
 app.secret_key = os.urandom(32) #create random key
 
-@app.route("/") #,methods=['GET', 'POST'])
-def disp_loginpage():
-    return render_template('child.html')
+from app import routes
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
