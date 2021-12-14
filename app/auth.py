a42d345279cc6cca4eafc073067043f495f149ed
Zhao Yu Lin <zhaoyu0305@gmail.com>
2021-12-14 08:40:44 -0500

    minor changes

--- app/auth.py
+++ app/auth.py
@@ -3,12 +3,21 @@ import sqlite3
 DB_file = "discobandit.db"
 
 
+def createTables():
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS
+    c.execute("""CREATE TABLE IF NOT EXISTS
+    c.execute("""CREATE TABLE IF NOT EXISTS 
+    db.commit()
+    db.close()
+
 def register(template_name, username, password, user_ID, user_password database):
     error = "ERROR: "
     error += validate(user_ID, request.args[username])
     error += validate(user_password, request.args[password])
     if (error == "ERROR: "):
-        #if userID is valid, store in database
+        #if userID and password is valid, store in database
         session[user_ID] = request.args[username]
         insert(database, request.args[username], request.args[password])
 
@@ -17,3 +26,12 @@ def register(template_name, username, password, user_ID, user_password database)
 
     return render_template(template_name, error = error)
     # return render_template('response.html', user = session.get("userID"))
+
+#not useful rn
+# def insert(table_name, username, password): #insert user and password into table
+#     with sqlite3.connect(DB_FILE) as db:
+#             #open if file exists, otherwise create
+#             c = db.cursor()
+#             c.execute("INSERT INTO " + table_name + "(username,password) VALUES (?,?)",(username,password) )
+#             db.commit()
+#             msg = "Record successfully added"
