14bc030fa625b759b44a52433fe3db48c9a779d9
Owen Yaggy <owyaggy@icloud.com>
2021-12-24 02:13:56 -0500

    finished weather page with modified api call

--- app/api.py
+++ app/api.py
@@ -2,13 +2,6 @@
 import requests
 from datetime import datetime
 
-def time_converter(utc):
-    dt = utc
-    dt = datetime.fromtimestamp(dt)
-    dt = str(dt)[12:16]
-    # print(dt)
-    return dt
-
 def nasa_apod(count=1):
     """Returns the NASA astronomy picture of the day"""
     
@@ -38,39 +31,48 @@ def nasa_apod(count=1):
     # could potentially be customizable via function arguments (e.g. number of images)
     return info
 
-    
+def get_time(utc, minutes=True):
+    dt = utc
+    dt = datetime.fromtimestamp(dt)
+    if minutes:
+        dt = str(dt)[11:16]
+    else:
+        dt = str(dt)[11:14]
+    return dt
+
+def minute_forecast(minutes):
+    minute_start = -1
+    minute_end = -1
+    for minute in range(len(minutes)):
+        if minutes[minute]['precipitation'] == 0:
+            if minute_start != -1:
+                minute_end = minute
+        else:
+            if minute_start == -1:
+                minute_start = minute
+    if minute_start == -1:
+        return "There will be no precipitation for the next hour."
+    elif minute_end == -1:
+        return f"Precipitation will begin in {minute_start} minutes."
+    else:
+        return f"Precipitation will begin in {minute_start} minutes and end after {minute_end - minute_start} minutes."
+
 def weather_api(city):
     """Returns weather for the city"""
 
     with open("keys/weather_key.txt") as api_key:
         key = api_key.read().rstrip('\n')
-    units = "imperial"
 
-    print(f"http://api.openweathermap.org/data/2.5/weather?q={city}&units={units}&appid={key}")
+    units = "imperial" # other potential option metric
     api_request = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&units={units}&appid={key}")
     info = {}
 
     if api_request.status_code == 200:
-        print('success')
         # kelvin_temp = api_request.json()["main"]["temp"]
         # info["temp"] = int((kelvin_temp - 273.15) * 1.8 + 32.5)
-        info["description"] = api_request.json()["weather"][0]["description"][0].upper() + api_request.json()["weather"][0]["description"][1:] # capitalize first character
-        info['temp'] = int(api_request.json()['main']['temp'])
-        info['feels_like'] = int(api_request.json()['main']['feels_like'])
         info['temp_min'] = int(api_request.json()['main']['temp_min'])
         info['temp_max'] = int(api_request.json()['main']['temp_max'])
-        info['pressure'] = int(api_request.json()['main']['pressure'])
-        info['humidity'] = int(api_request.json()['main']['humidity'])
-        info['visibility'] = int(api_request.json()['visibility'] * 0.000621371) # convert meters to miles
-        info['wind_speed'] = api_request.json()['wind']['speed']
-        info['wind_deg'] = api_request.json()['wind']['deg']
-        info['wind_gust'] = api_request.json()['wind']['gust']
-        info['clouds'] = api_request.json()['clouds']['all'] # cloudiness %
-        info['icon'] = api_request.json()['weather'][0]['icon']
-        info['main'] = api_request.json()['weather'][0]['main']
         info['city'] = api_request.json()['name']
-        info['sunrise'] = time_converter(api_request.json()['sys']['sunrise'])
-        info['sunset'] = time_converter(api_request.json()['sys']['sunset'])
         info['lat'] = api_request.json()['coord']['lat']
         info['lon'] = api_request.json()['coord']['lon']
         if 'rain' in api_request.json().keys():
@@ -78,13 +80,57 @@ def weather_api(city):
         if 'snow' in api_request.json().keys():
             info['snow'] = api_request.json()['snow']
 
-    '''api_request = requests.get(f"https://api.openweathermap.org/data/2.5?lat={info['lat']}&lon={info['lon']}")
+    with open('keys/forecast_key.txt') as api_key:
+        key = api_key.read().rstrip('\n') # changes key to avoid making two requests per call
+
+    print(f"https://api.openweathermap.org/data/2.5/onecall?lat={info['lat']}&lon={info['lon']}&units={units}&appid={key}")
+    api_request = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={info['lat']}&lon={info['lon']}&units={units}&appid={key}")
     if api_request.status_code == 200:
-        print(api_request.json())'''
+        info["description"] = api_request.json()['current']["weather"][0]["description"][0].upper() + \
+                              api_request.json()["current"]['weather'][0]["description"][1:]  # capitalize first character
+        info['temp'] = int(api_request.json()['current']['temp'])
+        info['feels_like'] = int(api_request.json()['current']['feels_like'])
+        info['pressure'] = int(api_request.json()['current']['pressure'])
+        info['humidity'] = int(api_request.json()['current']['humidity'])
+        info['visibility'] = int(api_request.json()['current']['visibility'] * 0.000621371)  # convert meters to miles
+
+        info['wind_speed'] = api_request.json()['current']['wind_speed']
+        info['wind_deg'] = api_request.json()['current']['wind_deg']
+        info['wind_gust'] = api_request.json()['current']['wind_gust']
+        info['clouds'] = api_request.json()['current']['clouds']  # cloudiness %
+        info['icon'] = api_request.json()['current']['weather'][0]['icon']
+        info['main'] = api_request.json()['current']['weather'][0]['main']
+        info['sunrise'] = get_time(api_request.json()['current']['sunrise']).lstrip('0')
+        info['sunset'] = str(int(get_time(api_request.json()['current']['sunset']).lstrip('0')[:2]) - 12) + get_time(api_request.json()['current']['sunset'])[2:]
+        info['dew_point'] = api_request.json()['current']['dew_point']
+        info['uvi'] = api_request.json()['current']['uvi']
+        info['minutely'] = minute_forecast(api_request.json()['minutely'])
+        info['hourly'] = []
+        for hour in range(10):
+            info['hourly'].append({
+                'time': get_time(api_request.json()['hourly'][hour]['dt'], minutes=False),
+                'temp': int(api_request.json()['hourly'][hour]['temp']),
+                'description': api_request.json()['hourly'][hour]['weather'][0]['description'][0].upper() + api_request.json()['hourly'][hour]['weather'][0]['description'][1:],
+                'icon': api_request.json()['hourly'][hour]['weather'][0]['icon']
+            })
+            info['hourly'][-1]['time'] += "00"
+            if int(info['hourly'][-1]['time'][:2]) < 12:
+                info['hourly'][-1]['time'] += " AM"
+            else:
+                info['hourly'][-1]['time'] += "PM"
+            if info['hourly'][-1]['time'][:2] == '00':
+                info['hourly'][-1]['time'] = '12' + info['hourly'][-1]['time'][2:]
+        info['daily'] = []
+        for day in api_request.json()['daily']:
+            info['daily'].append({
+                'date': str(datetime.fromtimestamp(day['dt']))[5:10],
+                'moon_phase': day['moon_phase'],
+                'min': int(day['temp']['min']),
+                'max': int(day['temp']['max']),
+                'description': day['weather'][0]['description'][0].upper() + day['weather'][0]['description'][1:],
+                'icon': day['weather'][0]['icon'],
+                'uvi': day['uvi']
+            })
+        info['daily'][0]['date'] = 'Today'
+
     return info
-
-#print(weather_api("New+York+City"))
-
-'''
-{'coord': {'lon': -74.006, 'lat': 40.7143}, 'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky', 'icon': '01d'}], 'base': 'stations', 'main': {'temp': 36.28, 'feels_like': 36.28, 'temp_min': 33.24, 'temp_max': 39.29, 'pressure': 1021, 'humidity': 46}, 'visibility': 10000, 'wind': {'speed': 1.99, 'deg': 266, 'gust': 5.99}, 'clouds': {'all': 1}, 'dt': 1640286600, 'sys': {'type': 2, 'id': 2039034, 'country': 'US', 'sunrise': 1640261859, 'sunset': 1640295164}, 'timezone': -18000, 'id': 5128581, 'name': 'New York', 'cod': 200}
-'''
